58caf334013596e5d37ac10ef800f2f9f63cff40
Emil Placheta <emil.minoru@gmail.com>
2020-05-10 18:57:34 +0100

    day6, added UI to the app

--- WOW2/main.js
+++ WOW2/main.js
@@ -1,46 +1,47 @@
 const $result = document.getElementById('result');
+const $total = document.getElementById('total');
+const $fairShare = document.getElementById('fairShare');
+const $submitButton = document.getElementById('submit-button');
+const $resultContainer = document.getElementById('resultContainer');
 
-/* const whatJudySpent = Number(document.getElementById('whatJudySpent').value);
+const whatJudySpent = Number(document.getElementById('whatJudySpent').value);
 const whatLydiaSpent = Number(document.getElementById('whatLydiaSpent').value);
 const whatEmilSpent = Number(document.getElementById('whatEmilSpent').value);
-const total = (whatJudySpent + whatLydiaSpent + whatEmilSpent).toFixed(2); */
-
-const group = [
-  {
-    name: 'Judy',
-    amountSpent: 50
-  },
-  {
-    name: 'Lydia',
-    amountSpent: 450
-  },
-  {
-    name: 'Emil',
-    amountSpent: 10
-  }
-];
 
 const owed = [];
 const owe = [];
+const group = [
+  { name: 'Judy', amountSpent: whatJudySpent },
+  { name: 'Lydia', amountSpent: whatLydiaSpent },
+  { name: 'Emil', amountSpent: whatEmilSpent }
+];
 
 const total = group.reduce((acc, num) => acc + num.amountSpent, 0);
 const fairShare = Number((total / group.length).toFixed(2));
-console.log(group.length);
-console.log(`Total amount spent: ${total}`);
-console.log(`Fair share : ${fairShare}`);
 
 group.forEach(el => {
   el.balance = (el.amountSpent - fairShare).toFixed(2);
 });
 
-const wow2 = function(group) {
+const calculate = function(group) {
   group.forEach(el =>
     el.balance > 0
-      ? owed.push(`${el.name} ${el.balance}`)
-      : owe.push(`${el.name} ${el.balance}`)
+      ? owed.push(`${el.name} should get back £${el.balance} `)
+      : owe.push(` ${el.name} needs to pay back £${el.balance}`)
   );
 };
-console.log(group);
-wow2(group);
-console.log(`Who is owed: ${owed}`);
-console.log(`From who and how much ${owe}`);
+//Calculate and display
+
+$submitButton.addEventListener('click', e => {
+  e.preventDefault();
+  calculate(group);
+  $resultContainer.classList.remove('hide');
+  $submitButton.classList.add('disabled');
+
+  $total.innerHTML = total;
+  $fairShare.innerHTML = fairShare;
+  $result.innerText = `
+    ${owed}
+    ${owe} 
+  `;
+});
